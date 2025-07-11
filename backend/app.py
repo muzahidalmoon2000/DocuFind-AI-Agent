4058c4cafc3f3d52377dd1ac6cfd01032be6f18a
muzahidalmoon2000 <muzahidalmoon@outlook.com>
2025-07-11 09:58:45 +0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -279,4 +279,5 @@ def serve_react(path):
         return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
